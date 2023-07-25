2db815ee5b5ea2dca54ed3f5618458bd707fa0d0
Isaac Akileng <isaacakileng56@gmail.com>
2023-07-25 18:13:06 +0300

    Add opd identifier

--- omod/src/main/java/org/openmrs/module/ugandaemr/htmlformentry/OPDFormSubmissionAction.java
+++ omod/src/main/java/org/openmrs/module/ugandaemr/htmlformentry/OPDFormSubmissionAction.java
@@ -2,6 +2,10 @@ package org.openmrs.module.ugandaemr.htmlformentry;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.Patient;
+import org.openmrs.PatientIdentifierType;
+import org.openmrs.PatientIdentifier;
+import org.openmrs.api.PatientService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.htmlformentry.CustomFormSubmissionAction;
 import org.openmrs.module.htmlformentry.FormEntryContext.Mode;
@@ -13,6 +17,7 @@ import org.openmrs.util.OpenmrsUtil;
 
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Enrolls patients into DSDM programs
@@ -23,8 +28,12 @@ public class OPDFormSubmissionAction implements CustomFormSubmissionAction {
 
     @Override
     public void applyAction(FormEntrySession session) {
+        Patient patient = session.getPatient();
         UgandaEMRService ugandaEMRService = Context.getService(UgandaEMRService.class);
+        PatientService patientService = Context.getPatientService();
+        PatientIdentifierType opdpatientIdentifierType = patientService.getPatientIdentifierTypeByUuid("be8f222a-3751-4f93-8c74-3aff399c19b6");
         Mode mode = session.getContext().getMode();
+
         if (!(mode.equals(Mode.ENTER) || mode.equals(Mode.EDIT))) {
             return;
         }
@@ -35,6 +44,18 @@ public class OPDFormSubmissionAction implements CustomFormSubmissionAction {
             ugandaEMRService.processDrugOrdersFromEncounterObs(session, true);
         }
 
+        // Check if the patient has no OPD Number, Then create it
+        PatientIdentifier OPDPatientIdentifer = patient.getPatientIdentifier(opdpatientIdentifierType);
 
+        if(OPDPatientIdentifer == null) {
+            Integer CurrentVisitNumber = ugandaEMRService.getPatientCurrentVisit(patient).getVisitId();
+
+            PatientIdentifier newIdentifier = new PatientIdentifier();
+            newIdentifier.setIdentifierType(opdpatientIdentifierType);
+            newIdentifier.setIdentifier(CurrentVisitNumber.toString());
+            patient.addIdentifier(newIdentifier);
+
+            Context.getPatientService().savePatient(patient);
+        };
     }
 }
