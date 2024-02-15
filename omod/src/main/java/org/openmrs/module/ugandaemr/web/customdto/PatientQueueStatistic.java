d6f2e49daaa43f50ce9743eac54921dce3607f48
slubwama1 <lubwamasamuel3@gmail.com>
2024-02-15 12:45:52 +0300

    U4x-308 Add endpoints for POC sites activated as well as Patients served vs Service Points

---
+++ omod/src/main/java/org/openmrs/module/ugandaemr/web/customdto/PatientQueueStatistic.java
@@ -0,0 +1,48 @@
+package org.openmrs.module.ugandaemr.web.customdto;
+
+import org.openmrs.LocationTag;
+import org.openmrs.Order;
+import org.openmrs.module.patientqueueing.model.PatientQueue;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class PatientQueueStatistic implements Serializable {
+    LocationTag locationTag;
+
+    Integer pending;
+    Integer serving;
+    Integer completed;
+
+    public LocationTag getLocationTag() {
+        return locationTag;
+    }
+
+    public void setLocationTag(LocationTag locationTag) {
+        this.locationTag = locationTag;
+    }
+
+    public Integer getPending() {
+        return pending;
+    }
+
+    public void setPending(Integer pending) {
+        this.pending = pending;
+    }
+
+    public Integer getServing() {
+        return serving;
+    }
+
+    public void setServing(Integer serving) {
+        this.serving = serving;
+    }
+
+    public Integer getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Integer completed) {
+        this.completed = completed;
+    }
+}
